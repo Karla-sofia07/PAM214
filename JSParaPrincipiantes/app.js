fadf7abd71af9cd5b862854fa10fc06356ed1ea5
Karla-sofia07 <124050136@upq.edu.mx>
2025-09-10 18:50:46 -0600

    avance 2

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,1 +1,14 @@
-document.write("<h1>Hello World</h1>");
+// tipos de datos
+"Hello world" // string
+'Hello world' // string
+//number
+100000
+-2.3
+//boolean
+true
+false
+// array
+["Joe", "Ryan", "martha"]
+[1,2,3]
+[true, false, true, false]
+//objet
