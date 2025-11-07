207cfb0f5faf493a171435a210eb25a341072370
Karla-sofia07 <124050136@upq.edu.mx>
2025-11-07 15:31:14 -0600

    Terminado

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,14 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ImageBackground, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ImageBackground, SafeAreaView, Button, Alert, TextInput } from 'react-native';
 
 export default function App() {
   const [showSplash, setShowSplash] = useState (true);
-  const[nombre, setNombre]= useState('');
+  const [nombre, setNombre]= useState('');
   const [profesion, setProfesion]= useState('');
-  const [biografia, setBiografia]= useState('');
   const [Correo, setCorreo]= useState ('');
   const [numero, setNumero]= useState ('');
+  const [refreshing, setRefreshing] = useState(false);
+
+   const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  };
+
+  const mostrarAlerta= () => {
+    if([nombre.trim(), profesion.trim(), Correo.trim(), numero.trim()].includes ('')) {
+     Alert.alert('Error,Por favor ingrese todos los datos');
+     alert ('Error, ingrese todos los datos');
+
+    } else{
+      Alert.alert('¡Hola, ${nombre}! tus datos han sido registrados correctamente');
+      alert ('¡Hola, ${nombre}! tus datos han sido registrados correctamente');
+      setNombre('')
+    }
+
+  };
   
   useEffect (() => {
 
@@ -35,6 +55,7 @@ export default function App() {
         style={styles.background} 
       >
         <View style={styles.overlay}>
+         <View style={styles.container}>
           <Text style= {styles.label}>Ingrese su nombre:</Text>
 
             <TextInput
@@ -45,6 +66,7 @@ export default function App() {
             keyboardType='default'
             autoCapitalize='words'
             />
+
           <TextInput
             style={styles.input}
             placeholder="Ingeniería"
@@ -53,14 +75,13 @@ export default function App() {
             keyboardType='default'
             autoCapitalize='words'
             />
-            <TextInput
+
+            <TextInput 
             style={styles.input}
-            placeholder="Biografía"
-            value={biografia}
-            onChangeText={setBiografia}
-            keyboardType='default'
-            autoCapitalize='words'
+            multiline={true}
+            numberOfLines={3}
             />
+
             <TextInput
             style={styles.input}
             placeholder="Correo"
@@ -69,28 +90,16 @@ export default function App() {
             keyboardType='default'
             autoCapitalize='words'
             />
+
             <TextInput
             style={styles.input}
             placeholder="numero de telefono"
-            value={nombre}
-            onChangeText={setNombre}
-            keyboardType='default'
-            autoCapitalize='words'
+            value={numero}
+            onChangeText={setNumero}
+            keyboardType='numeric'
             />
-            <TextInput 
-            style={styles.input}
-            placeholder="Password"
-            keyboardType="numeric" 
-            secureTextEntry={true}
-            />
-                
-            <TextInput 
-            style={styles.input}
-            multiline={true}
-            numberOfLines={4}
-            />
-
-          <Button title="Saludar" onPress={mostrarAlerta} color= '#df2db9ff' />
+          <Button title="Editar Perfil" onPress={mostrarAlerta} color= '#df2db9ff' />
+          </View>
         </View>
       </ImageBackground>
     </SafeAreaView>
@@ -131,11 +140,31 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
 
-
   title: {
     fontSize: 28,
     color: '#fff',
     marginBottom: 10,
     textAlign: 'center',
   },
+    container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  input: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1, //Sirve para ver el borde del input
+    borderRadius: 8, // Bordes redondeados
+    paddingHorizontal: 15, // Espacio interno a los lados
+    marginBottom: 20, // Espacio debajo del input
+    fontSize: 16,
+  },
 });
