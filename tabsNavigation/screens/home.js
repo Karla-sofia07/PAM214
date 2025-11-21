9b7c2a2ed190a7a3b8d9ae1f76b8bf5d310af995
Karla-sofia07 <124050136@upq.edu.mx>
2025-11-20 22:58:43 -0600

    Tabs & Stack: completada

--- tabsNavigation/screens/home.js
+++ tabsNavigation/screens/home.js
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+ import { Ionicons } from '@expo/vector-icons';
 
 export default function Home() {
   return (
